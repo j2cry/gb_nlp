880fc822514903c8e95873c511d6ce865f5f1439
avagadro <p-avagadro@yandex.com>
2022-12-15 22:45:59 +0300

    final: prepare

---
+++ final/final.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained('tinkoff-ai/ruDialoGPT-small')
+model = AutoModelForCausalLM.from_pretrained('tinkoff-ai/ruDialoGPT-small')
+
+
+inputs = tokenizer('@@ПЕРВЫЙ@@ привет @@ВТОРОЙ@@ привет @@ПЕРВЫЙ@@ как дела? @@ВТОРОЙ@@', return_tensors='pt')
+generated_token_ids = model.generate(
+    **inputs,
+    top_k=10,
+    top_p=0.95,
+    num_beams=3,
+    num_return_sequences=3,
+    do_sample=True,
+    no_repeat_ngram_size=2,
+    temperature=1.2,
+    repetition_penalty=1.2,
+    length_penalty=1.0,
+    eos_token_id=50257,
+    max_new_tokens=40
+)
+context_with_response = [tokenizer.decode(sample_token_ids) for sample_token_ids in generated_token_ids]
+print(context_with_response)
